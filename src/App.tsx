8179645793659d0afc8e012dd7ba3289e9b873f6
Dave Bryan <loresto.dbn@gmail.com>
2021-10-05 21:38:52 +0800

    Added mochabricks calculator

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,25 @@ function App() {
     }
   }
 
+  function computeTotalPrice(e: React.ChangeEvent<HTMLInputElement>) {
+    const priceEl = document.getElementById('price') as HTMLInputElement;
+    const weightEl = document.getElementById('weight') as HTMLInputElement;
+    const totalEl = document.getElementById('total_price');
+
+    const price = parseInt(priceEl.value);
+    const weight = parseInt(weightEl.value);
+
+    if (price && weight) {
+      let priceInPHP = price / 2;
+      let shippingFee = weight * 1.2;
+      let tax = (priceInPHP + shippingFee) * 0.18;
+
+      if (totalEl) {
+        totalEl.textContent = (priceInPHP + shippingFee + tax).toString();
+      }
+    }
+  }
+
   return (
     <div className="App">
       <div id="top" style={{paddingTop: '10vh'}}></div>
@@ -33,6 +52,22 @@ function App() {
           <p>Currently, I tried making different components like this header, a todo list, and some random elements like a regex-controlled input</p>
         </article>
       </section>
+      <section>
+        <h1>MochaBricks Pasabuy Price Calculator</h1>
+        <p>
+          Current calculation is done by: <br />
+          Price is converted to peso (price in yen / 2) <br />
+          Shipping Fee is weight multiplied by 1.2 <br />
+          Tax is equal to price (in peso) plus shipping fee multiplied by 0.18 <br />
+          Total Price = (Price/2) + (Weight*1.2) + ((Price/2) + (Weight*1.2) * 0.18)
+        </p>
+        <div>
+          <label htmlFor="price">Price (in Yen): </label><input onChange={computeTotalPrice} type="number" id="price" placeholder="price in Yen" /><br/>
+          <label htmlFor="weight">Price (in grams): </label><input onChange={computeTotalPrice} type="number" id="weight" placeholder="weight in grams" /><br/>
+        </div>
+        <p>Total Price: <span id="total_price"></span></p>
+      </section>
+      <hr />
       <section className="main-section" id="components">
         <h1>Components</h1>
         <div>
